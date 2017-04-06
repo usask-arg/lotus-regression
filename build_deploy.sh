c29e5352989f9fd2793f609d102682e1cb439507
Daniel Zawada <zawadadaniel@gmail.com>
2017-04-06 08:16:45 -0600

    Deploy fixing

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,14 +1,11 @@
 PACKAGE_VERSION="$(python -c "import LOTUS_regression; print(LOTUS_regression.__version__)")"
 
-conda env create --file docs/environment.yml > /dev/null 2>&1
-source activate doc_env
 python setup.py install
 
 python setup.py bdist_wheel
 
-cp dist/*.whl "$OUTPUT_DIR/$PACKAGE_VERSION/
-
-mkdir -p "$OUTPUT_DIR/$PACKAGE_VERSION/
+mkdir -p "$OUTPUT_DIR/$PACKAGE_VERSION/"
+cp dist/*.whl "$OUTPUT_DIR/$PACKAGE_VERSION/"
 
 cd docs
 make html
