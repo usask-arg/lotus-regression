7f737166db90213d2bf86658fbc38292e88708a5
Daniel Zawada <zawadadaniel@gmail.com>
2017-06-28 08:51:23 -0600

    Update predictor file

--- LOTUS_regression/predictors/__init__.py
+++ LOTUS_regression/predictors/__init__.py
@@ -1,3 +1,7 @@
+from LOTUS_regression.predictors import download
+import os
+
+
 def load_data(filename):
     import pandas as pd
     import os
@@ -8,3 +12,21 @@ def load_data(filename):
 
     return data[(data.index > '1977')]
 
+
+def remake_example_data():
+    pred = download.load_linear(1997)
+
+    pred['enso'] = download.load_enso(2)
+
+    pred['trop'] = download.load_trop(True)
+
+    pred['solar'] = download.load_solar()
+
+    pred['qbo_pcaA'] = download.load_qbo(3)['pca']
+    pred['qbo_pcaB'] = download.load_qbo(3)['pcb']
+    pred['qbo_pcaC'] = download.load_qbo(3)['pcc']
+
+    pred.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'predictors.csv'))
+
+if __name__ == "__main__":
+    remake_example_data()
