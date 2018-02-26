db9c0b27fe72aa518d07d7ef116d76c52e7e1cf3
Daniel Zawada <daniel.zawada@usask.ca>
2018-02-26 09:18:47 -0600

    Update CI to use the new location

--- build_deploy.sh
+++ build_deploy.sh
@@ -4,13 +4,15 @@ python setup.py install
 
 python setup.py bdist_wheel
 
-mkdir -p "$OUTPUT_DIR/$PACKAGE_VERSION/"
-cp dist/*.whl "$OUTPUT_DIR/$PACKAGE_VERSION/"
+[ -z "$CI_COMMIT_TAG" ] && OUTPUT_WHEEL_DIR="$OUTPUT_DIR""dev/"
+
+cp dist/*.whl "$OUTPUT_WHEEL_DIR/"
 
 cd docs
 mkdir -p source/images
 python make_regression_images.py
 make html
 
-mkdir -p "$OUTPUT_DIR/$PACKAGE_VERSION/docs/"
-cp -r build/html/* "$OUTPUT_DIR/$PACKAGE_VERSION/docs/"
+[ -z "$CI_COMMIT_TAG" ] && OUTPUT_DOC_DIR="$OUTPUT_DOC_DIR""dev/"
+
+cp -r build/html/* "$OUTPUT_DOC_DIR"
