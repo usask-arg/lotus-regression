1d49c4a558eaac9d8e2b88c78cb3852a2a4a4c4e
Daniel Zawada <daniel.zawada@usask.ca>
2024-06-13 14:44:07 -0600

    Move project to pyproject.toml (#11)
    * Move project to pyproject.toml
    
    * Update CI to use pip
    
    * Try again
    
    * Fix CI files
    
    * Update
    
    * Remove old versioneer things
    
    * Update sphinx version getting
    
    * Update predictors to 2024-05
    
    * Fix predictors
    
    * Add shell to wheel build
    
    * Add fetch depth to wheel build

---
+++ src/LOTUS_regression/plotting/trends.py
@@ -0,0 +1,155 @@
+from __future__ import annotations
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def pre_post_with_confidence(
+    data,
+    x="mean_latitude",
+    y="altitude",
+    pre_name="linear_pre",
+    post_name="linear_post",
+    figsize=(10, 6),
+    clim=10,
+    contour=True,
+    ylim=None,
+    log_y=False,
+    x_label=None,
+    y_label=None,
+    pre_title=None,
+    post_title=None,
+    c_label=None,
+):
+    fig = plt.figure(figsize=figsize)
+
+    plt.subplot(1, 2, 1)
+
+    plot_with_confidence(data, pre_name, x, y, clim, contour=contour)
+    if ylim is not None:
+        plt.ylim(ylim)
+
+    if x_label is not None:
+        plt.xlabel(x_label)
+
+    if y_label is not None:
+        plt.ylabel(y_label)
+
+    if pre_title is not None:
+        plt.title(pre_title)
+
+    if log_y:
+        plt.yscale("log")
+    plt.subplot(1, 2, 2)
+
+    im = plot_with_confidence(data, post_name, x, y, clim, contour=contour)
+    if ylim is not None:
+        plt.ylim(ylim)
+
+    if x_label is not None:
+        plt.xlabel(x_label)
+
+    if y_label is not None:
+        plt.ylabel(y_label)
+
+    if post_title is not None:
+        plt.title(post_title)
+
+    if log_y:
+        plt.yscale("log")
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.025, 0.7])
+    if c_label:
+        fig.colorbar(im, cax=cbar_ax, label=c_label)
+    else:
+        fig.colorbar(im, cax=cbar_ax)
+
+
+def plot_with_confidence(
+    data, name, x="mean_latitude", y="altitude", clim=10, contour=True
+):
+    levels = np.concatenate(([-clim * 50], np.linspace(-clim, clim), [clim * 50]))
+
+    if len(data[y].to_numpy()) == np.shape(data[name].to_numpy())[1]:
+        vals = data[name].to_numpy().T
+    else:
+        vals = data[name].to_numpy()
+
+    if contour:
+        C = plt.pcolor(
+            data[x].to_numpy(), data[y].to_numpy(), vals, cmap="RdBu_r", alpha=0
+        )
+
+        im = plt.contourf(
+            data[x].to_numpy(), data[y].to_numpy(), vals, cmap="RdBu_r", levels=levels
+        )
+    else:
+        C = plt.pcolor(data[x].to_numpy(), data[y].to_numpy(), vals, cmap="RdBu_r")
+        im = C
+
+    plt.clim(-clim, clim)
+    # Convert to 95%
+    error = data[name + "_std"] * 2
+
+    significant = np.abs(data[name]) > error
+
+    for _idx, p in enumerate(C._paths):
+        mean_lat = np.nanmean(np.unique(p._vertices[:, 0]))
+        mean_alt = np.nanmean(np.unique(p._vertices[:, 1]))
+
+        mask = significant.sel({x: mean_lat, y: mean_alt}, method="nearest").to_numpy()
+
+        if ~mask:
+            # Hatching doesnt seem to work very well
+            # ax.add_patch(Polygon(p._vertices, hatch='xxxx', color='blue', alpha=0.2, lw=0, fill=False))
+
+            lats = np.unique(p._vertices[:, 0])
+            alts = np.unique(p._vertices[:, 1])
+
+            plt.plot(lats, alts, "k", linewidth=0.1)
+            plt.plot(lats, alts[::-1], "k", linewidth=0.1)
+    return im
+
+
+def post_with_confidence(
+    data,
+    x="mean_latitude",
+    y="altitude",
+    post_name="linear_post",
+    figsize=(10, 6),
+    clim=10,
+    contour=True,
+    ylim=None,
+    log_y=False,
+    x_label=None,
+    y_label=None,
+    post_title=None,
+    c_label=None,
+    fig=None,
+):
+    if fig is None:
+        fig = plt.figure(figsize=figsize)
+
+    im = plot_with_confidence(data, post_name, x, y, clim, contour=contour)
+    if ylim is not None:
+        plt.ylim(ylim)
+
+    if x_label is not None:
+        plt.xlabel(x_label)
+
+    if y_label is not None:
+        plt.ylabel(y_label)
+
+    if post_title is not None:
+        plt.title(post_title)
+
+    if log_y:
+        plt.yscale("log")
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.025, 0.7])
+    if c_label:
+        fig.colorbar(im, cax=cbar_ax, label=c_label)
+    else:
+        fig.colorbar(im, cax=cbar_ax)
---
+++ src/LOTUS_regression/predictors/__init__.py
@@ -0,0 +1,153 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from LOTUS_regression.predictors import download
+
+
+def load_data(filename):
+    file_path = Path(__file__).parent / "data" / filename
+
+    data = pd.read_csv(file_path, parse_dates=True, index_col="time")
+
+    return data[(data.index > "1977")]
+
+
+def remake_example_data():
+    pred = pd.DataFrame()
+    pred["enso"] = download.load_enso(2)
+
+    pred["trop"] = download.load_trop(True)
+
+    pred["solar"] = download.load_solar()
+
+    pred["qboA"] = download.load_qbo(3)["pca"]
+    pred["qboB"] = download.load_qbo(3)["pcb"]
+    pred["qboC"] = download.load_qbo(3)["pcc"]
+
+    pred.index.name = "time"
+
+    pred -= pred.mean()
+    pred /= pred.std()
+
+    pred["linear_pre"] = download.load_linear(1997)["pre"]
+    pred["linear_post"] = download.load_linear(1997)["post"]
+
+    pred.to_csv(Path(__file__).parent / "data" / "predictors.csv")
+
+
+def make_baseline_pwlt():
+    pred = pd.DataFrame()
+    pred["enso"] = download.load_enso(0)
+    # pred['trop'] = download.load_trop(True)
+    pred["solar"] = download.load_solar()
+    pred["qboA"] = download.load_qbo(2)["pca"]
+    pred["qboB"] = download.load_qbo(2)["pcb"]
+    pred["aod"] = download.load_glossac_aod()
+
+    pred.index.name = "time"
+
+    pred -= pred.mean()
+    pred /= pred.std()
+
+    pred["linear_pre"] = download.load_linear(1997)["pre"]
+    pred["linear_post"] = download.load_linear(1997)["post"]
+
+    pred["constant"] = np.ones(len(pred.index))
+
+    pred.to_csv(Path(__file__).parent / "data" / "pred_baseline_pwlt.csv")
+
+
+def make_baseline_ilt(include_gap_linear=False, adjust_for_contiunity=False):
+    pred = pd.DataFrame()
+    pred["enso"] = download.load_enso(0)
+    # pred['trop'] = download.load_trop(True)
+    pred["solar"] = download.load_solar()
+    pred["qboA"] = download.load_qbo(2)["pca"]
+    pred["qboB"] = download.load_qbo(2)["pcb"]
+    pred["aod"] = download.load_glossac_aod()
+
+    pred.index.name = "time"
+
+    pred -= pred.mean()
+    pred /= pred.std()
+
+    linear = download.load_independent_linear(
+        pre_trend_end="1997-01-01", post_trend_start="2000-01-01"
+    )
+
+    pred["linear_pre"] = linear["pre"]
+    pred["linear_post"] = linear["post"]
+    pred["pre_const"] = linear["pre_const"]
+    pred["post_const"] = linear["post_const"]
+    pred["gap_cons"] = linear["gap_const"]
+
+    if include_gap_linear:
+        pred["gap_linear"] = linear["gap_linear"]
+
+    if adjust_for_contiunity:
+        linear["gap_linear"] /= linear["gap_linear"].max()
+        pred = pred.drop(["gap_cons"], axis=1)
+        pred["pre_const"] -= linear["gap_linear"]
+        pred["pre_const"] += linear["gap_const"]
+        pred["post_const"] += linear["gap_linear"]
+
+    if not include_gap_linear:
+        if adjust_for_contiunity:
+            pred.to_csv(
+                Path(__file__).parent / "data" / "pred_baseline_ilt_continuous.csv"
+            )
+        else:
+            pred.to_csv(Path(__file__).parent / "data" / "pred_baseline_ilt.csv")
+    else:
+        pred.to_csv(Path(__file__).parent / "data" / "pred_baseline_ilt_linear_gap.csv")
+
+
+def make_baseline_eesc():
+    pred = pd.DataFrame()
+    pred["enso"] = download.load_enso(0)
+    # pred['trop'] = download.load_trop(True)
+    pred["solar"] = download.load_solar()
+    pred["qboA"] = download.load_qbo(2)["pca"]
+    pred["qboB"] = download.load_qbo(2)["pcb"]
+    pred["aod"] = download.load_glossac_aod()
+
+    pred.index.name = "time"
+
+    pred -= pred.mean()
+    pred /= pred.std()
+
+    pred[["eesc_1", "eesc_2"]] = download.load_orthogonal_eesc(
+        "/media/valhalla/LOTUS/proxies/EESC_Damadeo/EESC.txt"
+    )[["eesc_1", "eesc_2"]]
+
+    pred["constant"] = np.ones(len(pred.index))
+
+    pred.to_csv(Path(__file__).parent / "data" / "pred_baseline_eesc.csv")
+
+
+def make_extra_predictors():
+    pred = pd.DataFrame()
+    pred["giss_aod"] = download.load_giss_aod()
+    pred["tropopause_pressure"] = download.load_trop(True)
+
+    pred.index.name = "time"
+
+    pred -= pred.mean()
+    pred /= pred.std()
+
+    pred.to_csv(Path(__file__).parent / "data" / "pred_extra.csv")
+
+
+if __name__ == "__main__":
+    make_extra_predictors()
+    make_baseline_pwlt()
+    make_baseline_ilt()
+    make_baseline_ilt(True)
+    make_baseline_ilt(False, True)
+    # make_baseline_eesc()
+    # remake_example_data()
